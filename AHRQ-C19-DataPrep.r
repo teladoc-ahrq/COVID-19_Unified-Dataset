f688283e3ebf19d512f54cc783bad19a2e5dd625
daniellameeker <daniellameeker@M-100528.local>
2021-09-09 07:14:01 -0700

    repair stray chars

--- AHRQ-C19-DataPrep.r
+++ AHRQ-C19-DataPrep.r
@@ -2,23 +2,101 @@
 # CLONED THIS REPOSITORY
 # https://github.com/CSSEGISandData/COVID-19_Unified-Dataset
 # When you clone the repo, you can use SSH instead of http with this config
-# git config remote.origin.url git@github.com:teladoc-ahrq/COVID-19_Unified-Dataset.git
+# git configs remote.origin.url git@github.com:teladoc-ahrq/COVID-19_Unified-Dataset.git
 
+td_path<-"~/teladoc-ahrq/covid19_publication_rawdata_import.xlsx"
+td_st_wk<-"~/teladoc-prv/ProvWk_StateCount.csv"
 # ON MAC OS THIS IS REQUIRED TO RUN sqldf
 options(gsubfn.engine = "R") 
+#install.packages("tidyverse")
 library(sqldf)
 library(tidyverse)
+library(readxl)
+td_c19<-read_excel(td_path)
+td_wt<-read_csv(td_st_wk)
+td_wt<-sqldf("select consult_year, consult_month, consult_state,
+              sum(n) as n from td_wt
+              group by consult_state, consult_year, consult_month
+             
+             ")
+
+td_wt<-sqldf("select n, consult_year , consult_month, consult_state,
+             sum(n) over(partition by consult_year, consult_month)
+              as month_denom
+              from td_wt
+order by consult_year, consult_month, consult_state
+             "
+             )
+
+td_wt<-sqldf("select distinct *, n/month_denom from td_wt")
+
 JHU <- readRDS('COVID-19.rds')
+# This is really annoying and screws up partitioning
+JHU$DateCh<-as.character(JHU$Date)
+JHU<-sqldf("Select JHU.*, strftime('%Y', JHU.DateCh) as Year, strftime('%m', JHU.DateCh) as Month from JHU")
 LU <- read_csv('COVID-19_LUT.csv')
 
-
+# Get US Tested, Deaths, and Cases
 JHU_STATE<-sqldf("SELECT JHU.*, LU.NameID, LU.ISO2 from JHU inner join LU 
       on 
       (LU.id=JHU.id and LU.ISO1_2C='US' 
-      and (LU.Level = 'State' or Level='Country') and Source='JHU' 
-      and Type='Confirmed') 
-      order by jhu.id, jhu.date")
+      and (LU.Level = 'State' or Level='Country') and 
+      (
+        (Source='JHU' and Type='Confirmed') or
+        (Source='JHU' and Type='Deaths') or
+        (Source='CTP' and Type='Tested')
+      ))")
+# clear memory
+rm(JHU,LU)
+# Add moving averages
+JHU_STATE_MA<-sqldf (
+  "select J.*, 
+  avg(Cases_New) OVER ( PARTITION BY J.ID, J.Type
+                  ORDER BY J.Date asc
+                  RANGE BETWEEN 7 PRECEDINGß
+                  AND 0 FOLLOWING
+              ) AS MA_Type_New
+  FROM JHU_STATE as J
+  --where J.Type='Confirmed' and J.ID='US'
+  "
+)
+rm(JHU_STATE)
+JHU_STATE_MA<-sqldf(
+"select J.Date, J.Year, J.Month, J.ID, J.ISO2 as ST,
+  T.MA_Type_New as TestMA, T.Cases_New as Tested,
+  D.MA_Type_New as DeathsMA, D.Cases_New as Deaths,
+  C.MA_Type_New as CasesMA, C.Cases_New as Cases
+from JHU_STATE_MA as J
+join JHU_STATE_MA as T on 
+(T.ID=J.ID and T.Date=J.DAte and T.Type='Tested')
+join JHU_STATE_MA as C on 
+(T.ID=J.ID and T.Date=J.Date and C.Type='Confirmed')
+join JHU_STATE_MA as D on 
+(D.ID=J.ID and D.Date=J.Date and D.Type='Deaths')
+")
 
+save.image(file="C19.Rdata")
+
+TDJHU<-sqldf("select J.*, t.* from JHU_STATE_MA as J join td_path as t 
+             
+             
+             ")
+
+#write_csv(JHU_STATE_MA,'~/teladoc-prv/JHU/JHU_STATE')
+
+
+
+
+TDH_data_JHU <-sqldf("SELECT td.*,D.Deaths, D.Moving_Avg_Death, TestMA, Tested, DeathMA, Deaths, FROM TDH_data as td
+join JHU_STATE_MA as D
+T
+on TD.DATE=T.DATE"
+)
+
+TDH_data <- read_excel("~/teladoc-ahrq/TDdata/covid19_publication_rawdata_import_deaths_tests.xlsx")
+TDH_WT <- read_csv('~/teladoc-prv/ProvWk_StateCount.csv')
+
+#**************************************************************************#
 rm(JHU,LU)
 
 JHU_STATE_D<-sqldf("select J.*, 
@@ -55,6 +133,7 @@ JHU_STATE_D as J
 ORDER BY J.ID, J.Date"
 
 JHU_STATE_D<-sqldf(mvavg)
+
 
 avgmvd<-"select J.*, 
   (J.Moving_Avg_Cases - lag(J.Moving_Avg_Cases, 1, NULL) 
